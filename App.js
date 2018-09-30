4b28f292eb0b91d479b456d5c4d79381346ad65f
Abraham Sedri <abraham.sedri@gmail.com>
2018-09-30 15:23:24 +0100

    commited

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Font } from 'expo';
 
 export default class App extends React.Component {
+    state = {
+      fontLoaded: false
+    };
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'arciform': require('./assets/fonts/arciform.ttf'),
+    });
+
+    this.setState({ fontLoaded: true });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+  {
+    this.state.fontLoaded ? (
+      <Text style={styles.text}>
+        pomo
+      </Text>
+    ) : null
+  }
       </View>
     );
   }
@@ -14,8 +33,15 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ff6b81',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontFamily: "arciform",
+    fontSize: 64,
+    color: '#fff',
+    textAlign: "center",
+    margin: 10
   },
 });
